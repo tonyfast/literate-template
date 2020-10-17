2e2f30ee9d47c5c07837b0a2d9b44b31710fc381
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-10-17 09:57:01 -0400

    start on js module

---
+++ {{cookiecutter.name}}/{{ cookiecutter.js_module }}/src/plugin.ts
@@ -0,0 +1,25 @@
+// Copyright (c) 2020 {{ cookiecutter.author }}
+// Distributed under the terms of the BSD-3-Clause License
+
+import {
+  JupyterFrontEndPlugin,
+  JupyterFrontend,
+} from '@jupyterlab/application';
+
+import { PLUGIN_ID } from '.';
+
+function activate(app: JupyterFrontend) {
+  console.warn(`🚀 ${PLUGIN_ID} is active`, app);
+}
+
+/**
+ * Initialization data for the jupyterlab_robotmode extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  autoStart: true,
+  id: PLUGIN_ID,
+  requires: [],
+  activate,
+};
+
+export default [extension];
